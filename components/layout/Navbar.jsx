d3d0e680c17b270290dd9d4a0d6fb2d7f402e693
Ashmit Singh <ashmit8736@gmail.com>
2026-07-12 02:47:55 +0530

    Update Navbar logo text from Ashmit.Singh to Portfolio

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -30,7 +30,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <Link href="/" className="text-2xl font-black bg-gradient-to-r from-[#ff5a1f] to-orange-400 bg-clip-text text-transparent uppercase tracking-tight">
-            Ashmit.Singh
+            Portfolio
           </Link>
           
           {/* Desktop Menu */}
